76c85bba9d9c7a48d3d3e439037a7bc250956191
Arjit Mishra <arjitm787@gmail.com>
2022-09-22 17:25:39 +0530

    Angular 14 Lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import { BooksComponent } from './books/books.component';
+import {FormsModule} from "@angular/forms";
+import { BookComponent } from './book/book.component';
 
 
 @NgModule({
-  declarations: [AppComponent, BooksComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, BooksComponent, BookComponent],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from "../interfaces/Book";
+
 
 @Component({
   selector: 'app-books',
@@ -7,15 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  name = 'Clean code';
-  author = 'Robert D. Martin';
-  src = 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg';
+  books: Book[] = [{
+    name: 'clean code',
+    author: 'Robert D. Martin',
+    src: 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+    amount: 800
+  },
+    {
+      name: 'The Pragmatic programmer',
+      author: 'David ThOmas',
+      src: 'https://images-na.ssl-images-amazon.com/images/I/41HXiIojloL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+      amount: 500
+    }];
 
-  isDisabled = true;
+  cart: Book[] = [];
 
-  constructor() { }
+  isShowing: boolean = true;
 
-  ngOnInit(): void {
+
+  constructor() {
+    console.log('Books constructor');
   }
 
+  ngOnInit(): void {
+    console.log('Books onInit');
+  }
+
+  addToCart(bookEvent: Book) {
+    this.cart.push(bookEvent);
+  }
 }
