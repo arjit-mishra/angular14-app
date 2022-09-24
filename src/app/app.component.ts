03b25cc98f31f27e35234065e3f8f6a1964bbdb4
Arjit Mishra <arjitm787@gmail.com>
2022-09-24 09:49:49 +0530

    Angular 14 Lesson 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {initializeApp} from "firebase/app";
+import {firebaseConfig} from "./firebase.config";
+import {AuthService} from "./auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
-export class AppComponent{}
+export class AppComponent implements OnInit{
+
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    initializeApp(firebaseConfig);
+  }
+
+  isAuthenticate() : boolean {
+    return this.authService.isAuthenticated;
+  }
+
+  logOut() {
+    this.authService.logout();
+  }
+}
